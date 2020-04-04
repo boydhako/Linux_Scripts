497f0ddc3b08cf2be6a1123efe90578f264e687c
Boyd H. Ako <wordpress@boydhanaleiako.me>
2020-04-04 12:39:20 -0600

    Added wp-integrity-checker files

--- Bluehost/update-all-wp-sites.bash
+++ Bluehost/update-all-wp-sites.bash
@@ -1,27 +1,30 @@
 #!/bin/bash
-wpphar="/home4/boydhana/scripts/wp-cli.phar"
-wpcomp="/home4/boydhana/scripts/wp-completion.bash"
+wwwdir="$1"
+wpphar="$HOME/GIT/Linux_Scripts/Bluehost/wp-cli.phar"
 
 function PREP {
-	alias wp="$wpphar"
+	if [ -z "$wwwdir" ]; then
+		printf "Please state the www directory to update.\n"
+		exit 1
+	fi
 }
 function MAIN {
 	PREP
-	for cfg in $(find $HOME/www/ -type f -name "wp-config.php"); do
+	for cfg in $(find $wwwdir -type f -name "wp-config.php"); do
 		dir="$(dirname $cfg)"
 		printf "===\tUPDATING %s\t===\n\n" "$dir"
 		printf "Updating core files in %s.\n" "$dir"
-		/home4/boydhana/scripts/wp-cli.phar core update --path="$dir"
+		$HOME/GIT/Linux_Scripts/Bluehost/wp-cli.phar core update --path="$dir"
 		printf "Updating database for %s.\n" "$dir"
-		/home4/boydhana/scripts/wp-cli.phar core update-db --path="$dir"
+		$HOME/GIT/Linux_Scripts/Bluehost/wp-cli.phar core update-db --path="$dir"
 		printf "Updating plugins for %s.\n" "$dir"
-		/home4/boydhana/scripts/wp-cli.phar plugin update --all --path="$dir"
+		$HOME/GIT/Linux_Scripts/Bluehost/wp-cli.phar plugin update --all --path="$dir"
 		printf "Updating themes for %s.\n" "$dir"
-		/home4/boydhana/scripts/wp-cli.phar theme update --all --path="$dir"
+		$HOME/GIT/Linux_Scripts/Bluehost/wp-cli.phar theme update --all --path="$dir"
 		printf "Verifiying file checksums in %s.\n" "$dir"
-		/home4/boydhana/scripts/wp-cli.phar core verify-checksums --path="$dir"
+		$HOME/GIT/Linux_Scripts/Bluehost/wp-cli.phar core verify-checksums --path="$dir"
 		printf "Flushing cache for %s.\n" "$dir"
-		/home4/boydhana/scripts/wp-cli.phar cache flush --path="$dir"
+		$HOME/GIT/Linux_Scripts/Bluehost/wp-cli.phar cache flush --path="$dir"
 		printf "======\n\n\n" "$dir"
 	done
 }
