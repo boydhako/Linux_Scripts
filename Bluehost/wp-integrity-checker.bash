407fdabcbe720f8de42c1d3c8606b2872c7ac74e
Boyd H. Ako <wordpress@boydhanaleiako.me>
2020-04-04 12:40:06 -0600

    Added wp-integrity-checker files

---
+++ Bluehost/wp-integrity-checker.bash
@@ -0,0 +1,42 @@
+#!/usr/bin/bash -xv
+date="$(date)"
+wwwdir="$1"
+db="$2"
+
+function PREP {
+	if [ -z "$wwwdir" ]; then
+		printf "Please specify the directory to check.\n"
+		exit 1
+	fi
+	if [ -z "$db" ]; then
+		db="/home4/boydhana/GIT/Linux_Scripts/Bluehost/$(basename $0 | awk -F\. '{printf $1}').db"
+	fi
+	printf "Cheking %s ...\nDatabase:%s\n" "$wwwdir" "$db"
+	if [ ! -f "$db" ]; then
+		sqlite3 $db "CREATE TABLE FILEINFO(checksum TEXT PRIMARY KEY NOT NULL, file TEXT NOT NULL, date_added TEXT, date_modified TEXT);"
+		sqlite3 $db "CREATE TABLE BADINSERT(checksum TEXT NOT NULL, file TEXT NOT NULL, date_found TEXT, note TEXT);"
+	fi
+}
+
+function ADDNEWFILE {
+	for file in $(find $wwwdir -type f); do
+		chksum="$(shasum -a 512 $file | awk '{printf $1}')"
+		printf "Checking for %s.\n" "$file"
+		if [ "$(sqlite3 $db "SELECT checksum FROM FILEINFO WHERE file = \"$file\";" | wc -l)" -lt "1" ]; then
+			printf "Adding %s\n" "$file"
+			sqlite3 $db "INSERT INTO FILEINFO (checksum,file,date_added) VALUES(\"$chksum\",\"$file\",\"$date\");"
+			addstat="$?"
+			if [ "$addstat" == "19" ]; then
+				sqlite3 $db "INSERT INTO BADINSERT (checksum,file,date_found,note) VALUES(\"$chksum\",\"$file\",\"$date\",\"Checksum is not unique.\");"
+			elif [ "$addstat" != "0" ]; then
+				exit 1
+			fi
+		fi
+	done
+}
+
+function FILEINTCHK {
+	PREP
+	ADDNEWFILE
+}
+FILEINTCHK
